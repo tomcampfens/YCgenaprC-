335982efb9032368d4625c74802ae457281b1921
tomcampfens <tomcampfens97@gmail.com>
2023-04-12 12:47:32 +0200

    git commit 1st

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+using System.Linq.Expressions;
+
+Console.WriteLine("Hello, World!");
+
+game NewGame = new game();
+//NewGame.rating = 5;
+//NewGame.Name = "LOTR";
+//NewGame.costs = 26.95;
+NewGame.StelNameEnRatingIn("LOTR", 6);
+NewGame.StelAllesIn("LOTR2", 4, 23.95, "Steam", true);
+
+game G2 = new game();
+//G2.Name = "GTA6";
+//G2.rating = 10;
+//G2.costs = 12.95;
+//G2.Start();
+//G2.PrintRating();
+//G2.PrintCosts();
+G2.StelNameEnRatingIn("GTA6", 10);
+G2.StelAllesIn("GTA7", 100, 574.95, "Steam", false);
+
+//Console.WriteLine(NewGame.Name);
+//Console.WriteLine(G2.Name);
---
+++ ConsoleApp1/game.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    // access modifier
+    public class game
+    {
+        // rating   =
+        // name     = =
+        // platform = = = properties
+        // costs    = =
+        // sold out =
+        //access modifier
+        public String Name { get; set; }
+        public int rating { get; set; }
+        public double costs { get; set; }
+        public String Platform { get; set; }
+        public Boolean soldOut { get; set; }
+
+        public int Persons { get; set; }
+
+        // access modifier: return type (void)
+        public void Start()
+        {
+            // code block
+            //Console.WriteLine(Name + " wordt gestart...");
+        }
+        public void PrintRating()
+        {
+            // code block
+        }
+        public void PrintCosts()
+        {
+            // code block
+            Console.WriteLine("Dit spel kost $" + costs);
+            Console.BackgroundColor = ConsoleColor.Blue;
+        }
+
+        public void StelNameEnRatingIn(String nameGame, int rating123)
+        {
+            //Rating is property = waarde argument
+            Name = nameGame;
+            rating = rating123;
+            Console.WriteLine(Name + " wordt gestart...");
+            Console.WriteLine("De rating van dit spel is " + rating);
+
+        }
+        public void StelAllesIn(String nameGame, int rating123, double Waarde, string Waar, Boolean Uitverkocht)
+        {
+            //Rating is property = waarde argument
+            Name = nameGame;
+            rating = rating123;
+            costs = Waarde;
+            Platform = Waar;
+            soldOut = Uitverkocht;  
+            Console.WriteLine(Name + " wordt gestart...");
+            Console.WriteLine("De rating van dit spel is " + rating);
+            Console.WriteLine("De waarde van dit spel is $" + costs);
+            Console.WriteLine("Je kunt dit spel vinden op " + Platform);
+            Console.WriteLine("uitverkocht? " + soldOut);
+
+        }
+    }
+}
